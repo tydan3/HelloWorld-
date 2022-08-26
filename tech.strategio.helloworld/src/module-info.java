ec85bf0e1a5b3f598f5657eeec93c7c036594725
Daniel Ty <tydaniel270@gmail.com>
2022-08-26 10:37:21 -0700

    initial commit

---
+++ tech.strategio.helloworld/src/module-info.java
@@ -0,0 +1,2 @@
+module tech.strategio.helloworld {
+}
---
+++ tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
@@ -0,0 +1,10 @@
+package tech.strategio.helloworld;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
